06f3a70230515076c55b4db5f1d90e0410247b01
depechecode <scunchen@umich.edu>
2020-08-02 19:46:33 -0400

    make constant G private

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static double G = 6.67e-11;
+    private static double G = 6.67e-11;
     public void draw(){
         StdDraw.picture(this.xxPos,this.yyPos,"images/"+this.imgFileName);
     }
