3f17c2fdbb9ab67ff2232b9c64bd185aeaaa399d
depechecode <scunchen@umich.edu>
2020-08-15 16:56:59 -0400

    add another null check

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,9 +82,16 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null) {
+        if(A==null && B==null){
             return null;
         }
+        if (A == null) {
+            return B;
+        }
+        if(B==null){
+            return A;
+        }
+
         IntList L=A;
         while(L.rest != null){
             L=L.rest;
@@ -99,8 +106,14 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null) {
+        if(A==null && B==null){
             return null;
+        }
+        if (A == null) {
+            return B;
+        }
+        if(B==null){
+            return A;
         }
         IntList L=new IntList(A.first,null);
         IntList p=L;
